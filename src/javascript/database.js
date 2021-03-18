6833b4c3475fb68b96e8c5af8f4dfa9404af2bab
Luca Fabbian <luca.fabbian.1999@gmail.com>
2021-03-18 04:08:42 +0100

    Fixed bug

--- src/javascript/database.js
+++ src/javascript/database.js
@@ -63,7 +63,7 @@ const Session = function(book, defaultBookData){
           }
         }
       }
-      return localStorage.getItem('mage-session-last') || randomString()
+      return localStorage.getItem('mage-session-last') ||  randomString(IDLength)
     })()
     location.replace(`#msession=${sessionName}`) // Set dell'url
     
