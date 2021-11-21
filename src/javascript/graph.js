e02702d91e0f908e93f4b4858914f2e73eeb089f
Luca Fabbian <luca.fabbian.1999@gmail.com>
2021-11-21 23:24:55 +0100

    Fixed graphs

--- src/javascript/graph.js
+++ src/javascript/graph.js
@@ -1,11 +1,15 @@
-const workerURL = 'static/graphviz/lite.render.js';
-let viz = new Viz({ workerURL });
+const hpccWasm = window["@hpcc-js/wasm"];
+
 
 const sanitizeLabel = (text) => text.replace(/\//g, "\\").replace(/\"/g, '\"')
 
 const generateGraph = (book) => {
   const data = book.get()
-  let s = 'digraph{\n'
+  let s = `digraph{
+    graph [fontname="arial", fontsize=10]; 
+    node [fontname="arial", fontsize=12, style="rounded,filled", shape=box];
+    edge [fontname="arial", fontsize=12];
+`
   book.sortedKeys(data.chapters).forEach(key => {
     s += `${key} [label="${sanitizeLabel(book.fullTitle(key))}"]\n`
     book.linksTo(key).forEach(otherKey => {
@@ -17,9 +21,9 @@ const generateGraph = (book) => {
 
 
 // Return the src attribute of an img tag
-const graphToImg = async(book) => {
-  const element = await viz.renderImageElement(generateGraph(book))
-  return element.getAttribute('src');
+const graphToImg = (book) => {
+  console.log("generating graph..")
+  return hpccWasm.graphviz.layout(generateGraph(book), "svg", "dot")
 }
 
 export {generateGraph, graphToImg}
