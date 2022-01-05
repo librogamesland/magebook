15738cd271b5e6a602f0bc436e3589bd519cf18e
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-01-05 21:31:26 +0100

    Removed alphas

--- alpha2/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-workbox.setConfig({debug: false})
-
-workbox.routing.registerRoute(
-  /\//,
-  new workbox.strategies.StaleWhileRevalidate(),
-);
