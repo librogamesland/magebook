3261ac2110008fad6bd55dac62c1c9a686f73651
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-01-06 00:11:06 +0100

    Trying to build app with github actions

--- src/golang/build-app.sh
+++ src/golang/build-app.sh
@@ -3,16 +3,17 @@ cd "$(dirname "$(realpath "$0")")";
 
 echo "BUILDING DESKTOP APP"
 
-rm -f ./editor/build/*.map
+# Copy editor files in order to embed them inside golang
+cp -r ../../editor ./editor
 
 echo "Building for Linux..."
-GOARCH="amd64" GOOS="linux"   go build -ldflags="-s -w" -o dist/magebook-linux main.go && upx dist/magebook-linux
+GOARCH="amd64" GOOS="linux"   go build -ldflags="-s -w" -o ../../dist/magebook-linux main.go && upx ../../dist/magebook-linux
 
 echo "Building for Windows..."
-GOARCH="amd64" GOOS="windows" go build -ldflags="-s -w -H windowsgui" -o dist/magebook-windows.exe main.go && upx dist/magebook-windows.exe
+GOARCH="amd64" GOOS="windows" go build -ldflags="-s -w -H windowsgui" -o ../../dist/magebook-windows.exe main.go && upx ../../dist/magebook-windows.exe
 
 echo "Building for MacOs Darwin..."
-GOARCH="amd64" GOOS="darwin"  go build -ldflags="-s -w" -o dist/magebook-macos main.go
+GOARCH="amd64" GOOS="darwin"  go build -ldflags="-s -w" -o ../../dist/magebook-macos main.go
 
 echo "Done!"
 
