3b9924f780de57bdcef6bd9918c94a8a3755bd82
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-08-23 15:41:30 +0200

    Fixed logo bug

--- src/components/dialogs/About.svelte
+++ src/components/dialogs/About.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
   import manifest from '../../../package.json'
+  import logo from '../../assets/img/logo.png'
+
 
   export let params : null
   export let callback : (value: any) => void
@@ -10,7 +12,7 @@
 
 <div class="dialog">
   <h3>Magebook - v. {manifest.version}</h3>
-  <img src="../../assets/img/logo.png" alt="Magebook logo">
+  <img src={logo} alt="Magebook logo">
   <br>
   <p>{@html $_('about.text')}</p>
   <p>{$_('about.contact')}: <a target="_blank" rel="noopener" href="mailto:luca.fabbian.1999@gmail.com?subject=Magebook%20-%20Segnalazione">luca.fabbian.1999@gmail.com</a></p>
